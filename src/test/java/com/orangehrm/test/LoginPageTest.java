763f08df18ba9cd8e64410baf48502b1575bcbe7
DHONI183 <manish17nov95@gmail.com>
2025-09-18 20:18:10 +0530

    old commit

--- src/test/java/com/orangehrm/test/LoginPageTest.java
+++ src/test/java/com/orangehrm/test/LoginPageTest.java
@@ -21,7 +21,7 @@ public class LoginPageTest extends BaseClass{
 	}
 	
 	
-	@Test
+	@Test(dataProvider="validLoginData",dataProviderClass=DataProviders.class)
 	public void verifyValidLoginTest(String username , String password) {
 		ExtentManager.logStep("Navigating to Login Page entering username and password");
 		loginPage.login(username, password);
